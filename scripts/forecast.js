5f8ab4f3acf3b40ac13078e7d6c624ee2d898877
varun sharma <varun.sharma0906@gmail.com>
2026-07-29 22:21:41 +0530

    getting weather condition of the city

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -5,6 +5,14 @@ const getCity = async(city) => {
     const query = `?q=${city}&appid=${key}`;
     const response = await fetch(base + query);
     const data = await response.json();
-    console.log(data);
+    return data[0];
 };
-getCity('delhi');
+
+const getWeather = async(lat,lon) => {
+    const base = 'https://api.openweathermap.org/data/2.5/weather';
+    const query = `?lat=${lat}&lon=${lon}&appid=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+    return data;
+};
